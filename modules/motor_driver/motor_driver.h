f26e6c9cb4395944c393ffc4b1d8522db46d116e
Hung <hung.tran@student.fontys.nl>
2014-10-07 18:57:35 +0200

    initial interface header file ->motor_driver

--- modules/motor_driver/motor_driver.h
+++ modules/motor_driver/motor_driver.h
@@ -1,12 +1,38 @@
-#ifndef EXAMPLE_H
-#define EXAMPLE_H
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
 
-struct example_status {
-	int status;
+#include <linux/ioctl.h>
+
+
+struct motor_driver_data {
+	
+	int pwm_pin;
+	int pwm;
+	int direction_pin1;
+	int direction_pin2;
+	
 };
 
-#define THERMIOC_MAGIC			'E'
-#define EXAMPLE_SET_STATUS		_IOW(THERMIOC_MAGIC, 0, struct example_status)
-#define EXAMPLE_GET_STATUS		_IOR(THERMIOC_MAGIC, 1, struct example_status)
+struct encoder_info {
+	int motor_speed;
+	int direction;
+};
 
-#endif // EXAMPLE_H
+#define THERMIOC_MAGIC			'M'
+#define MOTOR_SETSPEED			_IOW(THERMIOC_MAGIC, 0, struct motor_driver_data)
+#define MOTOR_GETSPEED			_IOR(THERMIOC_MAGIC, 1, encoder_info)
+
+// =================================================================================
+// void setSpeed(struct motor_driver_data* arg)
+// Pre : 
+// Post: sets the speed of the motor
+// =================================================================================
+
+// =================================================================================
+// int getSpeed(struct encoder_info* arg)
+// Pre : 
+// Post: returns the speed of the motor
+// =================================================================================
+
+
+#endif // MOTOR_DRIVER_H
